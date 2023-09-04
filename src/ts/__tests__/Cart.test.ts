1ad4a72cd76b2248ee273c529c75d26b0b5cf145
Yury Vereskun <satriks@mail.ru>
2023-09-04 11:34:07 +0300

    done task2

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -1,5 +1,7 @@
 import Cart from '../service/Cart';
 import Movie from "../domain/Movie";
+import Book from "../domain/Book";
+import MusicAlbum from "../domain/MusicAlbum"
 
 test('new card should be empty', () => {
   const cart = new Cart();
@@ -9,19 +11,40 @@ test('new card should be empty', () => {
 
 test('new movie add', () => {
   const cart=  new Cart() ;
-  cart.add(new Movie(
-  4242,
-  'Мстители',
-  'The Avengers',
-  2012,
-  'США',
-  'Avengers Assemble!',
-  'фантастика, боевик, фентези, приключения',
-  1000,
-  137,
-  ))
+  cart.add(new Movie(4242,'Мстители','The Avengers',2012,'США','Avengers Assemble!','фантастика, боевик, фентези, приключения',1000,137,))
   
   expect(cart.items[0].constructor).toEqual(Movie)
   // expect(cart.items.length).toBe(1)
 
 })
+test('test total sum', () => {
+  const cart=  new Cart() ;
+  cart.add(new Movie(4242,'Мстители','The Avengers',2012,'США','Avengers Assemble!','фантастика, боевик, фентези, приключения',1000,137,))
+  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+  
+  expect(cart.total()).toEqual(3900)
+
+
+})
+test('test total sum with discount', () => {
+  const cart=  new Cart() ;
+  cart.add(new Movie(4242,'Мстители','The Avengers',2012,'США','Avengers Assemble!','фантастика, боевик, фентези, приключения',1000,137,))
+  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+  
+  expect(cart.totalWithDiscount(10)).toEqual(3510)
+})
+
+test('test del items from cart', () => {
+  const cart=  new Cart() ;
+  cart.add(new Movie(4242,'Мстители','The Avengers',2012,'США','Avengers Assemble!','фантастика, боевик, фентези, приключения',1000,137,))
+  cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+  cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+  
+  cart.del(4242)
+
+ expect(cart.items.length).toBe(2)
+})
+
+
--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -7,6 +7,17 @@ export default class Cart {
         this._items.push(item);
     }
 
+    total(): number {
+        return this._items.reduce((res, item) => res += item.price, 0)
+    }
+    totalWithDiscount(discount: number): number {
+        return this.total() *(100 - discount) / 100
+    }
+
+    del(id: number): void {
+        this._items=this._items.filter(_=>_.id!= id ) ;
+    }
+
     get items(): Buyable[] {
         return [...this._items]; 
     }
