1e77fb0762e0172f278ac62f07db46fe382a640b
Yury Vereskun <satriks@mail.ru>
2023-09-04 13:50:34 +0300

    fix indent

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -16,8 +16,8 @@ test('new movie add', () => {
   cart.add(new Movie(4242,'Мстители','The Avengers',2012,'США','Avengers Assemble!','фантастика, боевик, фентези, приключения',1000,137,))
   
   expect(cart.items[0].constructor).toEqual(Movie)
-
 })
+
 test('test total sum', () => {
   const cart=  new Cart() ;
   cart.add(new Movie(4242,'Мстители','The Avengers',2012,'США','Avengers Assemble!','фантастика, боевик, фентези, приключения',1000,137,))
@@ -25,9 +25,8 @@ test('test total sum', () => {
   cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
   
   expect(cart.total()).toEqual(3900)
-
-
 })
+
 test('test total sum with discount', () => {
   const cart=  new Cart() ;
   cart.add(new Movie(4242,'Мстители','The Avengers',2012,'США','Avengers Assemble!','фантастика, боевик, фентези, приключения',1000,137,))
@@ -45,16 +44,17 @@ test('test del items from cart', () => {
   
   cart.del(4242)
 
- expect(cart.items.length).toBe(2)
+  expect(cart.items.length).toBe(2)
 })
 
 test('minus from cart ', () => {
   const cart=  new Cart() ;
   cart.add(new Phone(4242,'Iphone',50000, 5))
   cart.minus(4242)
+  
   expect(cart.items[0].quantity).toEqual(4)
-
 })
+
 test('minus from cart when 1 quantity ', () => {
   const cart=  new Cart() ;
   cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
@@ -68,8 +68,8 @@ test('minus from cart undef ', () => {
   const cart=  new Cart() ;
   cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
   cart.minus(1001)
+ 
   expect(cart.items[0].quantity).toEqual(undefined)
-
 })
 
 test('test total whit quantity ', () => {
@@ -80,7 +80,6 @@ test('test total whit quantity ', () => {
   cart.minus(4242)
   cart.minus(1001)
 
-
   expect(cart.total()).toEqual(202000)
 })
 
@@ -89,7 +88,6 @@ test('minus from cart ', () => {
   cart.add(new Laptop(4343,'HP',50000, 5))
   
   expect(cart.items[0].constructor).toEqual(Laptop)
-
 })
 
 test('test add to cart ', () => {
